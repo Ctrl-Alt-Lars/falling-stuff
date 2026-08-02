da8b7011915e6a1b931a47e5237ee6a96b5ebac7
Lars Christian Raae <36758964+Ctrl-Alt-Lars@users.noreply.github.com>
2026-08-02 21:05:10 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,75 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Rosa":return img`
+ffff........ffff.................
+f99ff......ff99f.................
+f999ff....ff999f.................
+f9b99ffffff99b9f.................
+f9bb99999999bb9f.................
+f93b99999999b39f.................
+f93399999999339f.................
+f99399999999399f.................
+f99999991199999f.................
+f999ff91119ff99f........ffffff...
+f999ff91111ff99f.......f999999ff.
+f99991111111999f......f99999999f.
+f9991111fff1199f.....f9991119999f
+f999c11fff1199ff.....f9911111999f
+f999cc111111c9ff.....f911dd11199f
+f99999bb33cc99fff....fbddbbd1199f
+f999999b33c99999ffffffbbdbbb1199f
+f9999999bb9999999999999999bb1999f
+f999911119999999999999999999b999f
+f999111111999999999999999999999f.
+f99911111119999999999999999999ff.
+f99111111119999999999999999999f..
+f99111111111999999999999999999f..
+ff9111111111999999999999999999f..
+.f9111111111999999999999999999f..
+.ff111111111999999999999999999f..
+..ff11111111999999999999999999f..
+...f91111119999999999999999999f..
+...f99911119999999999999999999f..
+....f99ffff9999fffffffffff9999f..
+....f99f..f999f.....f99f.f9999f..
+....f99f..f999f.....f99f.ff9999f.
+....f99f..f99f.......f9f..ff999f.
+....f99f..f99f.......f99f..ff99f.
+....f99f..f99f.......f99f...f99f.
+....f99f..f99f......f999f...f99f.
+...f999f..f99f......ffff....ff9f.
+...f99f..f999f.............f999f.
+...ffff..f99ff.............f999f.
+.........ffff..............ffff..
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
